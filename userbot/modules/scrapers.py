22544f797fdd38c7ca714283c462fe5d27725ce3
Layer <ashenzar@protonmail.com>
2021-02-15 18:14:03 -0600

    scrapers tr: simplify output

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -382,17 +382,16 @@ async def _(event):
     if event.fwd_from:
         return
     if "trim" in event.raw_text:
-        # https://t.me/c/1220993104/192075
         return
     input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
-        lan = input_str or "en"
+        lan = input_str or "es"
     elif "|" in input_str:
         lan, text = input_str.split("|")
     else:
-        await event.edit("`.tr LanguageCode` as reply to a message")
+        await event.edit(".tr LanguageCode as reply to a message")
         return
     text = emoji.demojize(text.strip())
     lan = lan.strip()
@@ -400,15 +399,10 @@ async def _(event):
     try:
         translated = translator.translate(text, dest=lan)
         after_tr_text = translated.text
-        mono_tr_text = (("`{}`").format(after_tr_text))
+        tr_text = (("{}").format(after_tr_text))
         # TODO: emojify the :
         # either here, or before translation
-        output_str = """**TRANSLATED** from {} to {}
-{}""".format(
-            translated.src,
-            lan,
-            mono_tr_text
-        )
+        output_str = """{}""".format(tr_text)
         await event.edit(output_str)
     except Exception as exc:
         await event.edit(str(exc))
